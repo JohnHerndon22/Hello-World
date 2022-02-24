9e62efb7e09ef594827541da380fa23b853f0577
John Herndon <jlove2bike@gmail.com>
2022-02-24 16:51:14 -0500

    Update GreatCode.py

--- GreatCode.py
+++ GreatCode.py
@@ -1,4 +1,5 @@
 title: GreatCode.py 
 import pandas as pd
-print('this is my great code - E2E corrections - more of these)
+print('this is my great code - E2E corrections - better version of corrections")
+print('this code goes back to phase 3.....')
       
